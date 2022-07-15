bf98f253e4e7fa3cc36c785797903e3d2e7016f2
MinPan9585 <minpan9585@gmail.com>
2022-07-16 01:14:28 +0800

    First Commit (Project Upload)

---
+++ NeckFly0714/Assets/Butterfly/MatchPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Joint;
+    public Vector3 Offset;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Joint.transform.position + Offset;
+
+    }
+}
---
+++ NeckFly0714/Assets/Scripts/CameraRay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraRay : MonoBehaviour
+{
+    GameObject butterfly;
+    DOTweenPath butterflyPath;
+
+    void Awake(){
+        butterfly = GameObject.Find("ButterflyPath1");
+        butterflyPath = butterfly.GetComponent<DOTweenPath>();
+    }
+
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100f);
+
+        if(hits.Length>0){
+            butterflyPath.DOPlay();
+
+
+            //success, toggle on animation and vfx
+            if(hits.Length == 3){
+                //corresponding vfx, add score
+            }
+            else if(hits.Length == 2){
+                //corresponding vfx, add score
+            }
+            else if(hits.Length == 1){
+                //corresponding vfx, add score
+            }
+        }
+        
+        else{
+            //fail, toggle off anim and vfx
+            butterflyPath.DOTogglePause();
+        }
+    }
+}
