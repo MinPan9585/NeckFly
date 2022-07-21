9174ab01453c736a6bf1ea92628d2a4728456f29
MinPan9585 <minpan9585@gmail.com>
2022-07-21 22:40:44 +0800

    upload new project with version 2019.3.4

---
+++ NeckFly0721/Assets/Scripts/SetInitPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInitPosition : MonoBehaviour
+{
+    GameObject rig;
+    Vector3 offset = new Vector3 (0,0,0.8f);
+
+    void Awake()
+    {
+        rig = GameObject.Find("NRCameraRig");
+        transform.position = rig.transform.position + offset;
+    }
+}
